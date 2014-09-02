b6d52fe104da53cbbc522312c0a7718546a17e4e
Craig Smitham <craigsmitham@gmail.com>
2014-09-02 13:26:52 -0500

    first commit

---
+++ build.fsx
@@ -0,0 +1,9 @@
+// include Fake lib
+#r @"packages\FAKE\tools\FakeLib.dll"
+open Fake
+open System
+open Fake.AssemblyInfoFile
+
+let version = "0.1.0-pre"
+let projectName = "HelloNuget"
+
---
+++ src/HelloNuGet/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNuGet
+{
+    public class CrazyCoolNewIdea
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
